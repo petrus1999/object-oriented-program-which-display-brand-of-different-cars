09bf76eab46e77ca6e4eefdd2e4293ecacb480cf
petrus <pshai671@gmail.com>
2023-02-13 09:20:41 +0200

    Add project files.

---
+++ Car.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace object_oriented_program_which_display_brand_of__different_cars
+{
+    class Car
+    {
+        string brand;
+
+        // constructor
+        Car(string theBrand)
+        {
+            brand = theBrand;
+        }
+
+        // copy constructor
+        Car(Car c1)
+        {
+            brand = c1.brand;
+        }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿
+using object_oriented_program_which_display_brand_of__different_cars;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // call constructor
+        Car car1 = new Car("Bugatti");
+
+        Console.WriteLine("Brand of car1: " + car1.brand);
+
+        // call the copy constructor
+        Car car2 = new Car(car1);
+        Console.WriteLine("Brand of car2: " + car2.brand);
+
+        Console.ReadLine();
+    }
+}
